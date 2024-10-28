1892be3e60898d93068610bedcde11e1a7cfec27
Farhan Yahaya <yahyafarhan48@gmail.com>
2024-10-28 00:04:54 +0000

    feat: make react import not required

--- packages/create-xtensio-app/_template/javascript/popup/popup.jsx
+++ packages/create-xtensio-app/_template/javascript/popup/popup.jsx
@@ -1,5 +1,3 @@
-import React from "react"
-
 const PopupPage = () => {
   return <div>Welcome to the popup page</div>
 }
--- packages/create-xtensio-app/_template/typescript/popup/popup.tsx
+++ packages/create-xtensio-app/_template/typescript/popup/popup.tsx
@@ -1,5 +1,3 @@
-import React from "react"
-
 const PopupPage: React.FC = () => {
   return <div>Welcome to the popup page</div>
 }
